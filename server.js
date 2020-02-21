14501633acbed87bea08712933430e3eecc53547
agohorel <gohorelalex@gmail.com>
2020-02-21 13:20:39 -0500

    remove old routes from server.js

--- server.js
+++ server.js
@@ -1,78 +1,14 @@
 const express = require("express");
-const db = require("./model.js");
+const projectRouter = require("./routers/project-router.js");
+const resourceRouter = require("./routers/resource-router.js");
+const taskRouter = require("./routers/task-router.js");
 
 const port = 5000;
 const server = express();
 server.use(express.json());
 
-server.get("/resources", async (req, res) => {
-  try {
-    const resources = await db.find("resources");
-    res.status(200).json(resources);
-  } catch (error) {
-    res.status(500).json({ error: "server error" });
-  }
-});
-
-server.post("/resources", async (req, res) => {
-  try {
-    const [newResourceID] = await db.insert("resources", req.body);
-    const newResource = await db.findById("resources", newResourceID);
-    res.status(201).json(newResource);
-  } catch (error) {
-    res.status(500).json({ error: "server error" });
-  }
-});
-
-server.get("/projects", async (req, res) => {
-  try {
-    const projects = await db.find("projects");
-    res.status(200).json(projects);
-  } catch (error) {
-    res.status(500).json({ error: "server error" });
-  }
-});
-
-server.post("/projects", async (req, res) => {
-  try {
-    const [newProjectID] = await db.insert("projects", req.body);
-    const newProject = await db.findById("projects", newProjectID);
-    res.status(201).json(newProject);
-  } catch (error) {
-    res.status(500).json({ error: "server error" });
-  }
-});
-
-server.get("/tasks/:id", async (req, res) => {
-  try {
-    const tasks = await db.getFormattedTasks(req.params.id);
-    res.status(200).json(tasks);
-  } catch (error) {
-    res.status(500).json({ error: "server error" });
-  }
-});
-
-server.post("/tasks/:id", async (req, res) => {
-  try {
-    const [newTaskID] = await db.insert("tasks", req.body);
-    const newTask = await db.findById("tasks", newTaskID);
-    res.status(201).json(newTask);
-  } catch (error) {
-    res.status(500).json({ error: "server error" });
-  }
-});
-
-server.get("/projects/:id/detail", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const project = await db.findById("projects", id);
-    const tasks = await db.getTasks(id);
-    const resources = await db.getProjectResources(id);
-
-    res.status(200).json({ ...project, tasks, resources });
-  } catch (error) {
-    res.status(500).json({ error: "server error" });
-  }
-});
+server.use("/projects", projectRouter);
+server.use("/resources", resourceRouter);
+server.use("/tasks", taskRouter);
 
 server.listen(port, () => console.log(`server listening on port ${port}`));
