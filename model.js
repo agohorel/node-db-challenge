ff8d48b20864ca60cc90c01d142e2691cdf6014b
agohorel <gohorelalex@gmail.com>
2020-02-21 12:08:41 -0500

    add GET/POST routes for resources

--- model.js
+++ model.js
@@ -8,6 +8,12 @@ function find(table) {
   return db(table);
 }
 
+function findById(table, id) {
+  return db(table)
+    .where({ id })
+    .first();
+}
+
 // SELECT t.description as "Task Description", t.notes as "Task Notes", p.name as "Project Name", p.description as "Project Description" FROM projects AS p
 // JOIN tasks as t
 // ON p.id = t.project_id;
@@ -23,3 +29,5 @@ function getTasks(id) {
     .join("tasks as t", { "p.id": "t.project_id" })
     .where({ id });
 }
+
+module.exports = { find, findById, insert, getTasks };
--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const express = require("express");
+const db = require("./model.js");
 
-const server = express();
 const port = 5000;
+const server = express();
+server.use(express.json());
 
+server.get("/resources", async (req, res) => {
+  try {
+    const resources = await db.find("resources");
+    res.status(200).json(resources);
+  } catch (error) {
+    res.status(500).json({ error: "server error" });
+  }
+});
+
+server.post("/resources", async (req, res) => {
+  try {
+    const [newResourceID] = await db.insert("resources", req.body);
+    const newResource = await db.findById("resources", newResourceID);
+    res.status(201).json(newResource);
+  } catch (error) {
+    res.status(500).json({ error: "server error" });
+  }
+});
 
 server.listen(port, () => console.log(`server listening on port ${port}`));
